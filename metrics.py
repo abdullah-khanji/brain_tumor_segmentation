0d3ea88499a7c6df27e5b7bfa1535896d464ebbe
abdullah-khanji <abdullah.us1721@gmail.com>
2024-04-18 23:37:39 +0500

    update18424

--- metrics.py
+++ metrics.py
@@ -3,11 +3,14 @@ from tensorflow.keras import backend as B
 
 smooth=1e-15
 
+@tf.keras.utils.register_keras_serializable()
 def dice_coef(y_true, y_pred):
     y_true= tf.keras.layers.Flatten()(y_true)
     y_pred= tf.keras.layers.Flatten()(y_pred)
     intersection= tf.reduce_sum(y_true * y_pred)
     return (2. * intersection + smooth)/(tf.reduce_sum(y_true)+tf.reduce_sum(y_pred))
 
+
+@tf.keras.utils.register_keras_serializable()
 def dice_loss(y_true, y_pred):
     return 1.0- dice_coef(y_true, y_pred)
--- train.py
+++ train.py
@@ -10,3 +10,106 @@ from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPla
 from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 
+np.random.seed(42)
+tf.random.set_seed(42)
+SIZE= (128, 128)
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def load_dataset(path, split=0.2):
+    images= sorted(glob(os.path.join(path, 'images', '*.png')))
+    masks= sorted(glob(os.path.join(path, 'masks', '*.png')))
+    split_size= int(len(images)*split)
+    train_x, valid_x= train_test_split(images, test_size=split_size, random_state=42)
+    train_y, valid_y= train_test_split(masks, test_size=split_size, random_state=42)
+    
+    train_x, test_x= train_test_split(train_x, test_size=split_size, random_state=42)
+    train_y, test_y= train_test_split(train_y, test_size=split_size, random_state=42)
+    
+    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+
+
+def read_image(path):
+    path= path.decode()
+    x= cv2.imread(path, cv2.IMREAD_COLOR)
+    x= cv2.resize(x, SIZE)
+    x= x/255.0
+    x= x.astype(np.float32)
+    return x
+
+def read_mask(path):
+    path= path.decode()
+    x= cv2.imread(path, cv2.IMREAD_GRAYSCALE) ##(h, w)
+    x= cv2.resize(x, SIZE)##(h, w)
+    x= x/255.0                              ##(h, w)
+    x= x.astype(np.float32)                 ##(h, w)
+    x= np.expand_dims(x, axis=-1)           ##(h, w)
+    return x
+
+def tf_parse(x, y):
+    def _parse(x, y):
+        x= read_image(x)
+        y= read_mask(y)
+        return x, y
+    x, y= tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+    x.set_shape([SIZE[0], SIZE[1], 3])
+    y.set_shape([SIZE[0], SIZE[1], 1])
+    return x,y
+
+
+def tf_dataset(X, Y, batch=2):
+    dataset= tf.data.Dataset.from_tensor_slices((X, Y))
+    dataset= dataset.map(tf_parse)
+    dataset= dataset.batch(batch)
+    dataset= dataset.prefetch(10)
+    return dataset
+
+
+create_dir("files")
+
+#hyperparameter
+batch_size=16
+lr= 1e-4
+num_epochs=200
+model_path=os.path.join("files", "model.keras")
+csv_path= os.path.join("files", "log.csv")
+(train_x, train_y), (valid_x, valid_y), (test_x, test_y)=load_dataset('./data')
+
+print("train: {}- {}".format(len(train_x),len(train_y)))
+print("valid: {}- {}".format(len(valid_x),len(valid_y)))
+print("test: {}- {}".format(len(test_x),len(test_y)))
+
+
+train_dataset= tf_dataset(train_x, train_y, batch_size)
+valid_dataset= tf_dataset(valid_x, valid_y, batch_size)
+
+test_dataset= tf_dataset(test_x, test_y, 16)
+###Model
+
+model= build_unet((128, 128, 3))
+model.compile(loss=dice_loss, optimizer=Adam(lr), metrics=[dice_coef])
+
+callbacks= [
+    ModelCheckpoint(model_path, verbose=1, save_best_only=True),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+    CSVLogger(csv_path),
+    EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=False)
+]
+
+model.fit(
+    train_dataset, epochs=num_epochs,validation_data=valid_dataset,
+    callbacks=callbacks
+)
+
+model.save('model_k_frmt.keras')
+
+import tensorflow as tf
+
+new_model=tf.keras.models.load_model('model_k_frmt.keras',custom_objects={'dice_loss': dice_loss})
+
+loss, acc= new_model.evaluate(test_dataset, verbose=2)
+
+print(loss, acc)
